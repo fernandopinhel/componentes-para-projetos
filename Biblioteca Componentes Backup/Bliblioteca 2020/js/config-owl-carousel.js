986aeac64a8290e7012f4ccd0e6955b04a8bfe1b
Fernando Pinhel <fernandopinhelll@gmail.com>
2020-03-16 10:53:22 -0300

    Atualizando.
    Atualizando.

---
+++ Biblioteca Componentes Backup/Bliblioteca 2020/js/config-owl-carousel.js
@@ -0,0 +1,29 @@
+// Options do plugins https://owlcarousel2.github.io/OwlCarousel2/docs/api-options.html
+  $('.owl-carousel').owlCarousel({
+  loop:true,
+  nav:true,
+  dots: false,
+  margin:10,
+  slideBy: 4,
+  responsiveClass:true,
+  responsive:{
+      0:{
+        items:1,
+        nav:true
+      },
+      992:{
+        items:2,
+        nav:true
+      },
+      1000:{
+        items:3,
+        nav:true,
+        loop:false
+      },
+      1200:{
+        items:4,
+        nav:true,
+        loop:false
+      }
+    }
+  })
